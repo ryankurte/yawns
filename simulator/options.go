5c5ba53e0fe60c496377ccfc4ba04977043401bc
Ryan Kurte <ryankurte@gmail.com>
2017-03-14 00:04:49 +1300

    Adding args

--- simulator/options.go
+++ simulator/options.go
@@ -7,6 +7,9 @@ type Options struct {
 	Connector  string `short:"n" long:"connector" description:"Network connector to use"`
 	PCAPFile   string `short:"f" long:"pcap-file" description:"PCap Output File"`
 	PCAPStream string `short:"s" long:"pcap-stream" description:"PCap Output Stream"`
+	ReportFile string `short:"r" long:"report" description:"Report file to write"`
+	LogDir     string `short:"l" long:"log-dir" description:"Log file directory"`
+	RunClients string `short:"x" long:"run-clients" description:"Enable simulator client management and specify the command to run (ie. ./app {{server}} {{address}})"`
 }
 
 // DefaultOptions creates an options instance populated with the default options
